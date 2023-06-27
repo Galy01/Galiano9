fe97c0813c1d09260651ad54fe1f3bf00a74910b
Galy01 <126773554+Galy01@users.noreply.github.com>
2023-06-27 19:00:19 +0200

    salto e animazioni

---
+++ Galiano/Assets/PlayerScript/Player1.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    private float Move;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    private Animator anim;
+void Start()
+{
+    rb = GetComponent<Rigidbody2D>();
+    anim = GetComponent<Animator>();
+}
+
+     void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("ground")){
+            isGrounded = true;
+        }else{
+            isGrounded = false;
+        }
+    }
+
+void Update()
+{
+    Move = Input.GetAxis("Horizontal");
+    rb.velocity = new Vector2(Move * speed ,rb.velocity.y);
+    if(Move != 0 && isGrounded==true){
+        anim.SetBool("isWalking",true);
+    }else{
+        anim.SetBool("isWalking",false);
+    }
+
+
+    if(isGrounded==false && rb.velocity.y > 0){
+        anim.SetBool("isJumping",true);
+    }else if(isGrounded==false && rb.velocity.y < 0){
+        anim.SetBool("isFlying",true);
+    }else if(isGrounded == true){
+        anim.SetBool("isFlying",false);
+        anim.SetBool("isJumping",false);
+    }
+
+    if(Input.GetButtonDown("Jump") || Input.GetKeyDown("[0]")){
+        rb.AddForce(new Vector2(rb.velocity.x,jump));
+        isGrounded=false;
+    }
+
+    bool hasMovement = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+    if(hasMovement)
+    {
+        transform.localScale = new Vector2 (Mathf.Sign(rb.velocity.x), 1f); 
+    }
+
+}
+
+}
