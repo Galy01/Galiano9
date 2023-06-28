5727e9fb250554e71a1bd3a090deb23713bebdaf
Galy01 <126773554+Galy01@users.noreply.github.com>
2023-06-28 11:53:05 +0200

    Update Player1.cs

--- Galiano/Assets/PlayerScript/Player1.cs
+++ Galiano/Assets/PlayerScript/Player1.cs
@@ -35,16 +35,20 @@ void Update()
     }
 
 
+
+
     if(isGrounded==false && rb.velocity.y > 0){
         anim.SetBool("isJumping",true);
+        anim.SetBool("isFlying",false);
     }else if(isGrounded==false && rb.velocity.y < 0){
         anim.SetBool("isFlying",true);
+        anim.SetBool("isJumping",false);
     }else if(isGrounded == true){
         anim.SetBool("isFlying",false);
         anim.SetBool("isJumping",false);
     }
 
-    if(Input.GetButtonDown("Jump") || Input.GetKeyDown("[0]")){
+    if((Input.GetButtonDown("Jump") || Input.GetKeyDown("[0]")) && isGrounded==true){
         rb.AddForce(new Vector2(rb.velocity.x,jump));
         isGrounded=false;
     }
